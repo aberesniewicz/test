44cb78f1fcc4c857cdaa86021fb411b98bec566d
Artur <artur.beresniewicz@exorigo-upos.pl>
2013-04-15 16:21:23 +0200

    aber 1

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -14,6 +14,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // test aber
         // TODO code application logic here
     }
 }
