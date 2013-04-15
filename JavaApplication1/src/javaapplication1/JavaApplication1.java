804306fdf09e70bedf100ff7e225a1bfda60dc1d
Artur <artur.beresniewicz@exorigo-upos.pl>
2013-04-15 15:04:05 +0200

    aber 1

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -14,6 +14,6 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("no i jestem in branch");
+        System.out.print("no i jestem in branch in master!!!!!!!!!!");
     }
 }
